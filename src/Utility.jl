279eeec4151a2d0e5aff3468e6d4c5df86b90937
Ben Conrad <ben@benconrad.net>
2021-08-02 18:09:16 -0500

    again

--- src/Utility.jl
+++ src/Utility.jl
@@ -5,6 +5,7 @@ module Utility
     function iWrap(i::Integer, n::Integer)
         return ((i-1)%n)+1 #(things) to shift for 1-indexed arrays
     end
+
     function iNext(i::Integer, n::Integer)
         return iWrap(i+1,n)
     end
